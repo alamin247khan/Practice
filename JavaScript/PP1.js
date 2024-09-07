a4aa2d47bf25d995e4830dfc2157f0e269ed855a
Al Amin Khan <167293333+alamin247khan@users.noreply.github.com>
2024-09-07 03:09:57 -0600

    ja pp

---
+++ JavaScript/PP1.js
@@ -0,0 +1,8 @@
+
+let oranges = 500;
+let apple = 400;
+let cost = (500+400);
+let takaIHave = 1000;
+
+let nowIHave = takaIHave-cost;
+console.log(nowIHave)
---
+++ JavaScript/PP2.js
@@ -0,0 +1,9 @@
+let mathematics = 80;
+let biology = 90;
+let chemistry = 55;
+let physics = 60;
+let bangla = 80;
+
+let total = mathematics + biology + chemistry + physics + bangla;
+let average = total / 5;
+console.log(average);
